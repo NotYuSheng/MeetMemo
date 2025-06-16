be04a08143f6f02261542351052c9cbd29ae4e7f
The Prunerator <yeeteck05@gmail.com>
2025-06-16 11:26:21 +0800

    build: started on HTML display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
-import TextInterface from "./components/Transcribe";
 
 function App() {
+  const [fileNames, setFileNames] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/checkfiles")
+      .then(res => res.json())
+      .then(data => setFileNames(data))
+      .catch(err => console.error("Failed to fetch file names:", err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
           Meet Memo
       </header>
-      <TextInterface/>
+      <div className='Padded-text'>
+        <h1>All transcriptions</h1>
+
+      </div>
     </div>
   );
 }
